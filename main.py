b5377ab07e2092d15be94322359c047c0ee62e33
Thomas B <buvat.thomas.pro@mail.com>
2024-10-31 09:51:48 +0100

    merge patrick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Titre de l'application
 st.title('Mon Projet SQL Alchemy')
@@ -11,5 +12,8 @@ vgsales = pd.read_csv("data/vgsales.csv")
 
 st.dataframe(vgsales)
 
+names = vgsales['Name'].unique().sort_values(by=['Name'])
 
+print(names)
 
+st.dataframe(names)
