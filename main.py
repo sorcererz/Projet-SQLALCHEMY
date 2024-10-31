a4cb75de5d5b6b304c252466a538a9e996994e67
Thomas B <buvat.thomas.pro@mail.com>
2024-10-31 09:58:16 +0100

    merge patrick

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ vgsales = pd.read_csv("data/vgsales.csv")
 
 st.dataframe(vgsales)
 
-names = vgsales['Name'].unique().sort_values(by=['Name'])
+namesArray = vgsales['Name'].unique()
+# names = names.sort_values(by=['Name'])
+names_nparray = np.array(namesArray)
 
-print(names)
+df_names = pd.DataFrame(names_nparray, columns=['Name']).sort_values(by=['Name'])
+# df = namesDF
+print(df_names)
 
-st.dataframe(names)
+st.dataframe(df_names)
